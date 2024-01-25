9aa47866188c6744c87009210be5154c6bcf342a
arulguru6 <arulguru6@gmail.com>
2024-01-25 13:07:29 +0530

    main commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssociatelistingComponent } from './component/associatelisting/associatelisting.component';
+import { AddassociateComponent } from './component/addassociate/addassociate.component';
+import { MaterialModule } from './Material.module';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssociatelistingComponent,
+    AddassociateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
